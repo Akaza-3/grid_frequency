3c56e883b406b4bb4692f49e73f058e156fa264c
Akaza-3 <kotharisahil4@gmail.com>
2024-04-25 01:22:25 +0530

    few more changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,18 +2,18 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="flex min-h-screen  bg-[#1c1a1b] pt-64 pl-8">
-      <div className="w-full md:w-2/3 lg:w-4/5">
-        <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-5xl md:text-7xl text-white font-bold ">
-          Grid Frequency Predictor...
+    <div className="min-h-screen bg-[#1c1a1b] pt-24 pl-6 overflow-hidden">
+      <div className="w-full">
+        <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-2xl md:text-5xl text-white font-bold">
+          Framework for Grid Frequency Prediction...
         </h1>
         <p className="text-white pt-6 text-lg w-2/3">
-          The Grid Frequency Predictor is trying to solve the problem of
-          predicting the frequency via Machine Learning.{" "}
+          This website is a framework for visualizing Grid Frequency and Machine
+          Learning models.
         </p>
       </div>
-      <div className="w-1/2 lg:w-1/8">
-        <img src="/graph.jpeg" alt="graph GIF" className="w-full" />
+      <div className="flex justify-center items-center pt-16">
+        <img src="/graphgif.gif" alt="graph GIF" className="lg:h-[300px]  " />
       </div>
     </div>
   );
